3a4cd3ba46330b015650600b93461f53f9e373c1
Ri <ri.le@upstatement.com>
2021-03-21 14:28:55 -0400

    readme and shell script changes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Simple Lambda Proxy
 
+An AWS Lambda function for setting up an AWS API Gateway proxy API for
+[Buttondown](https://buttondown.email/settings/programming). Use case is to set
+up custom subscription forms without revealing your private Buttondown API key.
+
+Uses ZIP file deployment to use Axios/other NPM dependencies.
+
 [Deploy Lambda function from CLI](https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-awscli.html)
+
+Requires installing [the AWS-CLI](https://aws.amazon.com/cli/)
--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ echo "Creating execution role"
 FUNCTION_NAME=buttondown-proxy
 aws iam create-role --role-name ${FUNCTION_NAME} --assume-role-policy-document file://trust-policy.json
 aws iam attach-role-policy --role-name ${FUNCTION_NAME} --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+make build
 aws lambda create-function --function-name ${FUNCTION_NAME} \
     --zip-file fileb://function.zip --handler index.handler --runtime nodejs14.x \
     --role arn:aws:iam::${AWS_USER_ID}:role/lambda-ex
