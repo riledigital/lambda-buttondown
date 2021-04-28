4bd244afaf96334f31be0e3a996f520c494ea090
Ri <ri.le@upstatement.com>
2021-04-28 14:54:35 -0400

    reformat error handling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-magic-numbers */
+/* eslint-disable func-names */
+/* eslint-disable max-lines-per-function */
 const axios = require('axios').default;
 require('dotenv').config();
 
@@ -8,7 +11,7 @@ require('dotenv').config();
  *
  * Will succeed with the response body.
  */
-exports.handler = async (event) => {
+exports.handler = async function (event) {
   // Can only test with API Gateway
   const formData = JSON.parse(event.body);
   console.log('The whole event:');
@@ -19,7 +22,7 @@ exports.handler = async (event) => {
   try {
     const response = await axios.post(
       '/subscribers',
-{
+      {
         email: formData.email,
         metadata: formData.metadata,
         notes: formData.notes,
@@ -33,15 +36,31 @@ exports.handler = async (event) => {
           'Content-Type': 'application/json',
           Authorization: `Token ${process.env.BUTTONDOWN_SECRET}`
         }
-      });
+      }
+    );
 
     console.log(`Token ${process.env.BUTTONDOWN_SECRET}`);
 
     console.log(response);
+    if (response.statusCode === 400) {
+      return {
+        statusCode: response.statusCode,
+        headers: {
+          'Content-Type': 'text/plain',
+          'x-amzn-ErrorType': response.code
+        },
+        isBase64Encoded: false,
+        body: `${response.code}: ${response.message}`
+      };
+    }
+
     return {
       statusCode: 200,
-      body: response.data,
-      headers: { 'Content-Type': 'application/json' }
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      isBase64Encoded: false,
+      body: JSON.stringify(response.body)
     };
   } catch (error) {
     console.log(error);
